2b9181e04cf8025ec24d80ff87487c01334c7ace
adeliaaaa <monicadelia88@gmail.com>
2021-08-14 17:13:57 +0700

    Monic bikin kriteria vaksin

--- src/ayo-vaksin/src/App.js
+++ src/ayo-vaksin/src/App.js
@@ -2,12 +2,14 @@ import { CssBaseline } from "@material-ui/core";
 import './App.css'
 import { LokasiVaksin } from "./pages/LokasiVaksin/LokasiVaksin";
 import { TentangVaksin } from "./pages/TentangVaksin/TentangVaksin";
+import { Kriteria } from "./pages/KriteriaVaksin/kriteriavaksin";
 
 function App() {
   return (
     <>
       <CssBaseline>
         <TentangVaksin />
+        <Kriteria/>
         <LokasiVaksin />
       </CssBaseline>
     </>
